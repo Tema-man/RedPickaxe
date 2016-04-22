aa41a99f6116c43073ed81d9b54ecdfee0cd7358
Artemii Vishnevskii <temaa.mann@gmail.com>
2016-04-22 11:47:39 +0300

    added twitter fabric. Crashlytics and Answers

---
+++ app/src/main/java/com/cherryman/redpickaxe/App.java
@@ -0,0 +1,16 @@
+package com.cherryman.redpickaxe;
+
+import android.app.Application;
+
+/**
+ * @author Artemii Vishnevskii
+ * @author Temaa.mann@gmail.com
+ * @since 22.04.2016.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
