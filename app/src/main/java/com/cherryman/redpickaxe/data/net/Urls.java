e872b01a1579945c84d0c5ba69c71d4549b48379
Artemii Vishnevskii <Tema@app-plus.com>
2016-06-12 18:17:39 +0300

    changed urls

--- app/src/main/java/com/cherryman/redpickaxe/data/net/Urls.java
+++ app/src/main/java/com/cherryman/redpickaxe/data/net/Urls.java
@@ -5,10 +5,7 @@ package com.cherryman.redpickaxe.data.net;
  * @since 05.06.2016
  */
 public class Urls {
-
-    public static final String HOST = "http://redmine.meshsecret.com";
-
-    public static final String USERS = "/users.json";
-    public static final String MY_USER = "/users/current.json";
-    public static final String ISSUES = "/issues.json";
+    public static final String USERS = "users";
+    public static final String CURRENT = "current.json";
+    public static final String ISSUES = "issues.json";
 }
